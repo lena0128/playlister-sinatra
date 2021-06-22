02847221bfe6945b962603b03d0b39c221917e85
Lena Hou <lenahou2013@gmail.com>
2021-06-21 20:34:47 -0400

    commit tree 3427892118ede12e4eff88decd6ccdeb6dc5d3d3

---
+++ app/models/artist.rb
@@ -0,0 +1,10 @@
+require_relative './concerns/slugifiable.rb'
+
+class Artist < ActiveRecord::Base
+    has_many :songs
+    has_many :genres, through: :songs
+
+    include Slugifiable::SlugMethod
+    extend Slugifiable::FindBySlug
+
+end
